8fdbd99a2d0f20ab6b27bba63888e9c5a8114827
Andrew Donkin <andrew.donkin@gallagher.com>
2023-12-04 19:41:42 +1300

    training subdir

--- github_action/entrypoint.sh
+++ github_action/entrypoint.sh
@@ -13,7 +13,7 @@ sed -i \
     -e "s@%%GITHUB_RUN_NUMBER%%@$GITHUB_RUN_NUMBER@" \
     -e "s@%%GITHUB_SHA%%@$GITHUB_SHA@" \
     -e "s@%%DATE%%@$(date)@" \
-    rest_training.adoc
+    training/rest_training.adoc
 
 # $$ is probably "1"
 D=${RUNNER_TEMP}/adoc.$$
@@ -21,13 +21,13 @@ rm -rf $D
 mkdir -p $D
 
 echo Building single-page HTML version of AsciiDoc
-asciidoctor -r asciidoctor-diagram -o $D/index.html --verbose rest_training.adoc || true
+asciidoctor -r asciidoctor-diagram -o $D/index.html --verbose training/rest_training.adoc || true
 
 echo Building PDF from AsciiDoc
-asciidoctor-pdf -r asciidoctor-diagram -o $D/rest_training.pdf --verbose rest_training.adoc || true
+asciidoctor-pdf -r asciidoctor-diagram -o $D/rest_training.pdf --verbose training/rest_training.adoc || true
 
 echo Building multi-page HTML from AsciiDoc
-asciidoctor-multipage -r asciidoctor-diagram -D $D -o $D/paged.html --verbose rest_training.adoc || true
+asciidoctor-multipage -r asciidoctor-diagram -D $D -o $D/paged.html --verbose training/rest_training.adoc || true
 
 echo Putting output into $INPUT_OUTPUT
 tar --dereference -C $D -cvf $INPUT_OUTPUT --exclude .asciidoctor .
