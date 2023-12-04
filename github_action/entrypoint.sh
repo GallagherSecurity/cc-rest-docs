505fae99b2e36e9a70f0b7a1f08069f1f9a049d0
Andrew Donkin <andrew.donkin@gallagher.com>
2023-12-05 09:14:54 +1300

    Perms 1

--- github_action/entrypoint.sh
+++ github_action/entrypoint.sh
@@ -25,6 +25,7 @@ echo Building multi-page HTML from AsciiDoc
 asciidoctor-multipage -r asciidoctor-diagram -o paged.html --verbose training/rest_training.adoc || true
 
 echo Putting output into $INPUT_OUTPUT
+ls -al ref
 tar --dereference -cvf $INPUT_OUTPUT --exclude .asciidoctor --exclude .git --exclude .gitignore ref
 
 echo Artifact: $INPUT_OUTPUT
