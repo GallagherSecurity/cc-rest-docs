30dd20381400a3b04bdf50126d0fd7f710da1f99
Andrew Donkin <andrew.donkin@gallagher.com>
2023-12-04 19:29:39 +1300

    New GH Pages

---
+++ github_action/entrypoint.sh
@@ -0,0 +1,38 @@
+#! /bin/sh -l
+
+# Bail on error.
+set -e
+
+# Arguments:
+# $1 == $INPUT_OUTPUT :  filename for tarball artifact
+
+[[ x"$INPUT_OUTPUT" != x ]]
+
+sed -i \
+    -e 's@\[source,mermaid\]@[mermaid]@' \
+    -e "s@%%GITHUB_RUN_NUMBER%%@$GITHUB_RUN_NUMBER@" \
+    -e "s@%%GITHUB_SHA%%@$GITHUB_SHA@" \
+    -e "s@%%DATE%%@$(date)@" \
+    rest_training.adoc
+
+# $$ is probably "1"
+D=${RUNNER_TEMP}/adoc.$$
+rm -rf $D
+mkdir -p $D
+
+echo Building single-page HTML version of AsciiDoc
+asciidoctor -r asciidoctor-diagram -o $D/index.html --verbose rest_training.adoc || true
+
+echo Building PDF from AsciiDoc
+asciidoctor-pdf -r asciidoctor-diagram -o $D/rest_training.pdf --verbose rest_training.adoc || true
+
+echo Building multi-page HTML from AsciiDoc
+asciidoctor-multipage -r asciidoctor-diagram -D $D -o $D/paged.html --verbose rest_training.adoc || true
+
+echo Putting output into $INPUT_OUTPUT
+tar --dereference -C $D -cvf $INPUT_OUTPUT --exclude .asciidoctor .
+ls -alr $RUNNER_TEMP
+
+echo Artifact: $INPUT_OUTPUT
+ls -l $INPUT_OUTPUT
+
