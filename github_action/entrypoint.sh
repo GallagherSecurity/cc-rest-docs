860212388e7bdc5c2ddc60085a7175976a2933ae
Andrew Donkin <andrew.donkin@gallagher.com>
2025-10-01 12:39:39 +1300

    multi-page in same dir

--- github_action/entrypoint.sh
+++ github_action/entrypoint.sh
@@ -27,7 +27,10 @@ echo Building PDF from AsciiDoc
 asciidoctor-pdf -r asciidoctor-diagram -o $D/training/rest_training.pdf --verbose training/rest_training.adoc || true
 
 echo Building multi-page HTML from AsciiDoc
-asciidoctor-multipage -r asciidoctor-diagram -D $D/training/multipage -o $D/training/multipage/index.html --verbose training/rest_training.adoc || true
+# asciidoctor-multipage does not support -o properly:  https://github.com/owenh000/asciidoctor-multipage/issues/43
+# It expects the root HTML file to have the same basename as the adoc.
+cp training/rest_training.adoc training/multipage.adoc
+asciidoctor-multipage -r asciidoctor-diagram -D $D/training --verbose training/multipage.adoc || true
 
 # Tarball must be rooted at . otherwise deploy-pages fails, so copy everything in
 cp -r ref $D
